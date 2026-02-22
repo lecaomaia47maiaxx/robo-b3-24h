c9674bcbfbef812528daf5ccc736c5effb28ead6
lecaomaia47maiaxx <lecaomaia47@icloud.com>
2026-02-22 17:22:15 -0300

    Update b3_bot.py

--- b3_bot.py
+++ b3_bot.py
@@ -9,7 +9,7 @@ from telegram import Bot
 # ================= CONFIGURAÇÕES =================
 TOKEN = "8430351852:AAF50usp88gBEQ9XAlS98pOCVs8aBNztAqc"
 CHAT_ID = "8352381582"
-ALPHA_KEY = "OYSICYD1972XILCB"  # Removido espaço inicial
+ALPHA_KEY = "OYSICYD1972XILCB"  
 
 bot = Bot(token=TOKEN)
 
