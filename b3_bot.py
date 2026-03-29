cc95bf956b359602132a6c95a959dfb5e43fb7cb
lecaomaia47maiaxx <lecaomaia47@icloud.com>
2026-03-29 09:59:31 -0300

    Update b3_bot.py

--- b3_bot.py
+++ b3_bot.py
@@ -1,32 +1,33 @@
 import os
 from flask import Flask, request
-from telegram import Bot, Update
-from telegram.ext import Dispatcher, CommandHandler
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.environ.get("8430351852:AAF50usp88gBEQ9XAlS98pOCVs8aBNztAqc")
 URL = os.environ.get("https://robo-b3-24h-1.onrender.com")
 
 app = Flask(__name__)
-bot = Bot(token=TOKEN)
-dispatcher = Dispatcher(bot, None, use_context=True)
+application = Application.builder().token(TOKEN).build()
 
-def start(update, context):
-    update.message.reply_text("🤖 Bot B3 online 24h!")
+# COMANDO START
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🤖 Bot B3 online 24h!")
 
-dispatcher.add_handler(CommandHandler("start", start))
+application.add_handler(CommandHandler("start", start))
 
-@app.route('/')
-def home():
-    return 'Bot rodando 24h!'
-
-@app.route(f'/{TOKEN}', methods=['POST'])
+# WEBHOOK
+@app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
-    update = Update.de_json(request.get_json(force=True), bot)
-    dispatcher.process_update(update)
-    return 'ok'
+    update = Update.de_json(request.get_json(force=True), application.bot)
+    application.process_update(update)
+    return "ok"
 
+@app.route("/")
+def home():
+    return "Bot rodando 24h!"
+
+# INICIAR
 if __name__ == "__main__":
-    # MUITO IMPORTANTE PARA O RENDER
     port = int(os.environ.get("PORT", 10000))
-    bot.setWebhook(f"{URL}/{TOKEN}")
+    application.bot.set_webhook(f"{URL}/{TOKEN}")
     app.run(host="0.0.0.0", port=port)
